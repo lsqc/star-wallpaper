4ad666750c2dd42eacd684ee38c34b20317769d2
lsqc <lsqc@nya.vodka>
2025-08-26 02:14:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# star-wallpaper
+
+i needed a background and didn't want to use some random image from the internet.. so now i make my own random image?
---
+++ main.c
@@ -0,0 +1,86 @@
+#include "./raylib.h"
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(int argc, char* argv[])
+{
+    int COUNT = 400;
+
+    int HEIGHT = 1440;
+    int WIDTH = 3440;
+
+    char* e;
+
+    if (argc >= 3)
+    {
+        long h = strtol(argv[1], &e, 10);
+        if (*e != '\0')
+        {
+            printf("invalid height: %s\n", argv[1]);
+            return 1;
+        }
+
+        long w = strtol(argv[2], &e, 10);
+        if (*e != '\0')
+        {
+            printf("invalid width: %s\n", argv[1]);
+            return 1;
+        }
+
+        if (argc >= 4)
+        {
+
+            long c = strtol(argv[3], &e, 10);
+            if (*e != '\0')
+            {
+                printf("invalid count: %s\n", argv[3]);
+                return 1;
+            }
+            COUNT = c;
+        }
+
+        HEIGHT = h;
+        WIDTH = w;
+    }
+
+    InitWindow(WIDTH, HEIGHT, "window");
+
+    Vector2 stars[COUNT]; 
+
+    for (int i = 0; i < COUNT; i++)
+    {
+        Vector2 pos;
+        pos.x = rand() % WIDTH;
+        pos.y = rand() % HEIGHT;
+
+        stars[i] = pos;
+    }
+
+    while(!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        for (int i = 0; i < COUNT; i++)
+        {
+            DrawPixel((int) stars[i].x, (int)stars[i].y, PURPLE);
+        }
+
+        EndDrawing();
+
+        if (IsKeyPressed(KEY_S))
+        {
+            time_t now = time(NULL);
+            struct tm *t = localtime(&now);
+
+            TakeScreenshot("img.png");
+        }
+    }
+
+    CloseWindow();
+
+    return 0;
+}
